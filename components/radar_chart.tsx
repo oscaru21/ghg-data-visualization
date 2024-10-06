e66dc21bae0deecd06fc5347a14249692dd221d6
Oscar Umana <oscarumana1021@gmail.com>
2024-10-05 22:33:14 -0400

    test charts

---
+++ components/radar_chart.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { PolarAngleAxis, PolarGrid, Radar, RadarChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A radar chart"
+
+const chartData = [
+  { asset: "air-co2", mean: 186 },
+  { asset: "cmt-co2", mean: 305 },
+  { asset: "cmv-co2", mean: 237 },
+  { asset: "com-co2", mean: 273 },
+  { asset: "res-co2", mean: 209 },
+  { asset: "rrd-co2", mean: 214 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+export function Component() {
+  return (
+    <Card>
+      <CardHeader className="items-center pb-4">
+        <CardTitle>Arkansas</CardTitle>
+        <CardDescription>
+          Sources of CO2 emmisions in Arkansas in 2020
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <RadarChart data={chartData}>
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <PolarAngleAxis dataKey="asset" />
+            <PolarGrid />
+            <Radar
+              dataKey="mean"
+              fill="var(--color-desktop)"
+              fillOpacity={0.6}
+            />
+          </RadarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="flex items-center gap-2 leading-none text-muted-foreground">
+          January - June 2024
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
